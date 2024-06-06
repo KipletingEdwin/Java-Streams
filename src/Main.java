323fc3e0e4b729c89d3748f387ad5a4bf28be082
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-06 12:43:22 +0300

    finalize on streams

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        List<Person> people = new ArrayList<Person>();
+
+        people.add(new Person("Warren Buffett", 120));
+        people.add(new Person("Jeff Bezos", 150));
+        people.add(new Person("Bill Gates", 100));
+        people.add(new Person("Mark Zuckenberg", 50));
+
+//        List<Person> hundredClub = new ArrayList<>();
+//        List<Person> hundredClub =  people.stream()
+//                .filter(person -> person.billions >= 100)
+//                .collect(Collectors.toList());
+
+        List<Person> sortedList = people.stream()
+                .sorted(Comparator.comparing(person -> person.name))
+                        .collect(Collectors.toUnmodifiableList());
+
+//        for( Person p : people ){
+//            if(p.billions >= 100){
+//                hundredClub.add(p);
+//            }
+//        }
+
+        sortedList.forEach(person -> System.out.println(person.name));
+
+
+
+
+
     }
 }
